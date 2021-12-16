adbbb009cf11f5aff2954b2b829eb19b4c589d8c
Muhammad <Muhammad.medhat@gmaol.com>
2021-12-16 15:52:50 +0200

    creating the form method

--- md-forms/md-forms.php
+++ md-forms/md-forms.php
@@ -29,7 +29,69 @@ if(! class_exists(  'MDForms' )){
       
     public function enqueue_scripts(){}
 
-    public function form( $atts ){}
+    public function form( $atts ){
+        global $post;
+        $atts = shortcode_atts(
+            array(
+                'add_honeypot'=>false
+            ), $atts, 'mdform');
+
+        //1) Instantiate the class
+            $form = new PHPFormBuilder();
+
+        //2) Change any form attributes, if desired
+
+            $form->set_att( 'action', esc_url ( admin_url( 'admin-post.php') ) );
+            $form->set_att( 'add_honeypot', $atts['add_honeypot'] );
+
+        // 3) Add inputs, in order you want to see them
+        
+
+        //hidden fields
+        $form->add_input('action', array(
+            'type'=>'hidden', 
+            'value' =>'mdform'
+        ), 'action');
+        $form->add_input('wp_nonce', array(
+            'type'=>'hidden', 
+            'value' =>wp_create_nonce( 'submit_mdform')
+        ), 'wp_nonce');
+        $form->add_input('redirect_id', array(
+            'type'=>'hidden', 
+            'value' => $post->ID
+        ), 'redirect_id');
+
+
+        //input fields
+        $form->add_input('Name', array(
+            'type'=>'text', 
+            'placeholder'=>'your name', 
+            'required'=>true
+        ), 'name'); 
+        $form->add_input('Phone', array(
+            'type'=>'number', 
+            'placeholder'=>'your phone', 
+            'required'=>true
+        ), 'phone');
+        $form->add_input('Phone 2', array(
+            'type'=>'number', 
+            'placeholder'=>'your phone', 
+            'required'=>false
+        ), 'phone2');
+
+
+        //shortcode ahould not output directly
+        ob_start();
+        $status = filter_input(INPUT_GET, 'status', FILTER_VALIDATE_INT);
+        if($status == 1){
+            printf('<div class="message success">%s</div>', __('great', 'mdforms'));        
+        }
+
+        // build the form
+        $form->build_form();
+
+        return ob_get_clean();
+    }
 
     public function form_handler(){}
 
